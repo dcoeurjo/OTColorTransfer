4512745352d46855da7095cbeb011a4a80dbd2f4
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2019-08-29 11:10:22 +0200

    gpu

--- colorTransferGPUv0.cpp
+++ colorTransferGPUv0.cpp
@@ -14,15 +14,24 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+
+
+#include <boost/compute/core.hpp>
+#include <boost/compute/algorithm/sort.hpp>
+#include <boost/compute/algorithm/sort_by_key.hpp>
+#include <boost/compute/container/vector.hpp>
+#include <boost/compute/algorithm/iota.hpp>
+
+
 //Global flag to silent verbose messages
 bool silent;
 
-void slicedTransfer(std::vector<float> &source,
-                    const std::vector<float> &target,
-                    const int width,
-                    const int height,
-                    const int nbSteps,
-                    const int batchSize)
+void slicedTransfer(std::vector<double> &source,
+const std::vector<double> &target,
+const int width,
+const int height,
+const int nbSteps,
+const int batchSize)
 {
   //Random generator init to draw random line directions
   std::random_device rd{};
@@ -45,6 +54,17 @@ void slicedTransfer(std::vector<float> &source,
   auto lambdaProjSource = [&projsource](unsigned int a, unsigned int b) {return projsource[a] < projsource[b]; };
   auto lambdaProjTarget = [&projtarget](unsigned int a, unsigned int b) {return projtarget[a] < projtarget[b]; };
   
+  // get default OpenCL device and setup context
+  boost::compute::device device = boost::compute::system::default_device();
+  boost::compute::context context(device);
+  boost::compute::command_queue queue(context, device);
+  boost::compute::vector<float> projSourceGPU(width*height, context);
+  boost::compute::vector<float> projTargetGPU(width*height, context);
+  //Pixel Id
+  boost::compute::vector<unsigned int> idSourceGPU(width*height, context);
+  boost::compute::vector<unsigned int> idTargetGPU(width*height, context);
+  
+  
   for(auto i=0; i < idSource.size() ; ++i)
   {
     idSource[i]=i;
@@ -56,10 +76,10 @@ void slicedTransfer(std::vector<float> &source,
     for(auto batch = 0; batch < batchSize; ++batch )
     {
       //Random direction
-      float dirx = dist(gen);
-      float diry = dist(gen);
-      float dirz = dist(gen);
-      float norm = sqrt(dirx*dirx + diry*diry + dirz*dirz);
+      double dirx = dist(gen);
+      double diry = dist(gen);
+      double dirz = dist(gen);
+      double norm = sqrt(dirx*dirx + diry*diry + dirz*dirz);
       dirx /= norm;
       diry /= norm;
       dirz /= norm;
@@ -72,9 +92,27 @@ void slicedTransfer(std::vector<float> &source,
         projtarget[i] = dirx * target[3*i] + diry * target[3*i+1] + dirz * target[3*i+2];
       }
       
+      boost::compute::copy(projsource.begin(), projsource.end(), projSourceGPU.begin(), queue);
+      boost::compute::copy(projtarget.begin(), projtarget.end(), projTargetGPU.begin(), queue);
+      boost::compute::iota(idSourceGPU.begin(), idSourceGPU.end(), 0, queue);
+      boost::compute::iota(idTargetGPU.begin(), idTargetGPU.end(), 0, queue);
+      
+      queue.finish();
+      
       //1D optimal transport of the projections with two sorts
-      std::sort(idSource.begin(), idSource.end(), lambdaProjSource);
-      std::sort(idTarget.begin(), idTarget.end(), lambdaProjTarget);
+      boost::compute::sort_by_key(projSourceGPU.begin(),projSourceGPU.end(), idSourceGPU.begin(), queue);
+      boost::compute::sort_by_key(projTargetGPU.begin(),projTargetGPU.end(), idTargetGPU.begin(), queue);
+
+      queue.finish();
+      
+  
+      boost::compute::copy(projSourceGPU.begin(), projSourceGPU.end(), projsource.begin(), queue);
+      boost::compute::copy(projTargetGPU.begin(), projTargetGPU.end(), projtarget.begin(), queue);
+      boost::compute::copy(idSourceGPU.begin(), idSourceGPU.end(), idSource.begin(), queue);
+      boost::compute::copy(idTargetGPU.begin(), idTargetGPU.end(), idTarget.begin(), queue);
+      
+      queue.finish();
+      
       
       //We accumulate the displacements in a batch
       for(auto i = 0; i < idSource.size(); ++i)
@@ -109,9 +147,9 @@ int main(int argc, char **argv)
   app.add_option("-b,--sizeBatch", batchSize, "Number of dirtections on a batch (1)");
   bool applyRegularization = false;
   app.add_flag("-r,--regularization", applyRegularization, "Apply a regularization step of the transport plan using bilateral filter (false).");
-  float sigmaXY = 16.0;
+  double sigmaXY = 16.0;
   app.add_option("--sigmaXY", sigmaXY, "Sigma parameter in the spatial domain for the bilateral regularization (16.0)");
-  float sigmaV = 5.0;
+  double sigmaV = 5.0;
   app.add_option("--sigmaV", sigmaV, "Sigma parameter in the value domain for the bilateral regularization (5.0)");
   silent = false;
   app.add_flag("--silent", silent, "No verbose messages");
@@ -136,17 +174,17 @@ int main(int argc, char **argv)
     exit(1);
   }
   
-  std::vector<float> sourcefloat(width*height*nbChannels);
-  std::vector<float> targetfloat(width*height*nbChannels);
+  std::vector<double> sourcedouble(width*height*nbChannels);
+  std::vector<double> targetdouble(width*height*nbChannels);
 #pragma omp parallel for
   for(auto i = 0 ; i <width*height*nbChannels; ++i)
   {
-    sourcefloat[i] = static_cast<float>(source[i]);
-    targetfloat[i] = static_cast<float>(target[i]);
+    sourcedouble[i] = static_cast<double>(source[i]);
+    targetdouble[i] = static_cast<double>(target[i]);
   }
   
   //Main computation
-  slicedTransfer(sourcefloat, targetfloat, width, height, nbSteps, batchSize);
+  slicedTransfer(sourcedouble, targetdouble, width, height, nbSteps, batchSize);
   
   
   //Output
@@ -156,27 +194,27 @@ int main(int argc, char **argv)
     //Regularization of the transport plan (optional)
     // (bilateral filter of the difference)
     if (!silent) std::cout<<"Applying regularization step"<<std::endl;
-    cimg_library::CImg<float> transport(width, height, 1, 3);
+    cimg_library::CImg<double> transport(width, height, 1, 3);
     for(auto i=0; i<width*height; ++i)
     {
-      transport[i] = sourcefloat[3*i] - static_cast<float>(source[3*i]);
-      transport[i+ width*height] = sourcefloat[3*i+1] - static_cast<float>(source[3*i+1]);
-      transport[i+2*width*height] = sourcefloat[3*i+2] - static_cast<float>(source[3*i+2]);
+      transport[i] = sourcedouble[3*i] - static_cast<double>(source[3*i]);
+      transport[i+ width*height] = sourcedouble[3*i+1] - static_cast<double>(source[3*i+1]);
+      transport[i+2*width*height] = sourcedouble[3*i+2] - static_cast<double>(source[3*i+2]);
     }
     transport.blur_bilateral(transport, sigmaXY,sigmaV);
-  
+    
     auto output2(output);
     for(auto i = 0 ; i < width*height ; ++i)
     {
-      output[3*i]   = static_cast<unsigned char>(  std::min(255.0f, std::max(0.0f, static_cast<float>(source[3*i  ]) + transport[i])));
-      output[3*i+1] = static_cast<unsigned char>(  std::min(255.0f, std::max(0.0f, static_cast<float>(source[3*i+1]) + transport[i+ width*height])));
-      output[3*i+2] = static_cast<unsigned char>(  std::min(255.0f, std::max(0.0f, static_cast<float>(source[3*i+2]) + transport[i+ width*height*2])));
+      output[3*i]   = static_cast<unsigned char>(  std::min(255.0, std::max(0.0, static_cast<double>(source[3*i  ]) + transport[i])));
+      output[3*i+1] = static_cast<unsigned char>(  std::min(255.0, std::max(0.0, static_cast<double>(source[3*i+1]) + transport[i+ width*height])));
+      output[3*i+2] = static_cast<unsigned char>(  std::min(255.0, std::max(0.0, static_cast<double>(source[3*i+2]) + transport[i+ width*height*2])));
     }
   }
   else
   {
     for(auto i = 0 ; i < width*height*nbChannels ; ++i)
-      output[i] = static_cast<unsigned char>(  std::min(255.0f, std::max(0.0f,  sourcefloat[i])));
+      output[i] = static_cast<unsigned char>(  std::min(255.0, std::max(0.0,  sourcedouble[i])));
   }
   
   //Final export
